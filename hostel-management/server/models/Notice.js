0aa32b3ffb90399e1165c8191b0d4bca687414dd
Avishkar Pansare <87558645+avishkarspansare@users.noreply.github.com>
2026-06-12 22:37:56 +0530

    Delete hostel-management directory

--- hostel-management/server/models/Notice.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const noticeSchema = new mongoose.Schema({
-  title: { type: String, required: true, trim: true },
-  message: { type: String, required: true, trim: true },
-  postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Notice', noticeSchema);
